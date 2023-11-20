0c1ed7a388fa9621648900f4b7692e49dafca6a1
fortuinp <pinda.fortuin@gmail.com>
2023-11-20 15:06:04 +0200

    2011

--- script/code.js
+++ script/code.js
@@ -265,35 +265,199 @@
 //     return data
 // }
 
-function person(firstName,lastname,age,hrs,rate){
-    //creating and returning an object
-    return{
-        // name:firstName,
-        // surname:lastname,
-        // userAge :age,
-        // useRate : rate
-        firstName,lastname,age,hrs,rate
-    }
+// function person(firstName,lastname,age,hrs,rate){
+//     //creating and returning an object
+//     return{
+//         // name:firstName,
+//         // surname:lastname,
+//         // userAge :age,
+//         // useRate : rate
+//         firstName,lastname,age,hrs,rate
+//     }
+// }
+// let person1=person('Peter','Mukanya',3,60,500)
+// console.log(person1);
+// let person2=person('John','Mukanya',1,20,350)
+// console.log(person2);
+
+// //constructive function allows us to create an object.Then return this
+// //name of fuction must be uppercase
+// function Laptop(make,amount){
+//     this.make=make
+//     this.amount=amount
+//     // it does this(JS)==> return this
+// }
+
+// let laptop1= new Laptop ('HP',18000)
+// let laptop2= new Laptop('Dell',20000)
+// console.log(laptop1);
+// console.log(laptop2);
+
+// //check for DNA -- instanceof
+// console.log(laptop2 instanceof Laptop)
+
+// //dynamic nature of an object-- able to update and add new property on object
+// let person = {
+//     firstname:'john',
+//     lastname : 'bruce'
+// }
+// console.log(person);
+// //change value
+// person.firstname= 'Peter'
+// // square notation
+// //person['firstname']='Peter'
+// console.log(person);
+// //add a new property
+// person.age=20
+// console.log(person);
+
+
+// //constructor pr0perty is the same as instance of
+// function NewRecord(name){
+//     this.name=name
+// }
+// let person1= new NewRecord('john')
+// let person2= {name : 'henk'}
+// console.log(person2);
+// //constructor property
+// console.log(person1.constructor=== NewRecord); //true
+// //same as using instance of
+// console.log(person1 instanceof NewRecord); //TRUE
+
+// //functions are object
+// let person= {
+//     firstname: 'john',
+//     lastname:'bruce',
+//     fullname : function(){
+//         //make use of this. NB
+//         return `${this.firstname} ${this.lastname}`
+//     }
+
+// }
+// console.log(person);
+// console.log(person.fullname());
+
+// let person={
+//     firstname: 'john',
+//     lastname:'bruce',
+//     hrs: 120,
+//     rate :500,
+//     salary :function(){
+//         return this.hrs*this.rate
+//     }
+// }
+// console.log(person);
+// console.log(person.salary());
+// //return holds value for you so no need to log you can just do the below to call it like eventlistener and all
+// person.salary()
+
+// //looping through an object , we loop to save time of repeating ourselves
+// let Laptop={  
+//     make : 'dell',
+//     cpu: '3GHZ',
+//     ram :'16GB'
+// }
+// console.log(Laptop);
+// for (const key in Laptop){
+//     console.log(Laptop[key]);
+// }
+// //to display value too
+// console.log(Laptop);
+// for (const key in Laptop){
+//     console.log(Laptop[key,'',Laptop[key]]);
+// }
+
+//this below(of) will give an error, we need to use for in , that deals with index which is also key or property
+// console.log(Laptop);
+// for (const key of Laptop){
+//     console.log(Laptop[key]);
+// }
+
+//can also use object or foreach(cam) but for objects for in is cleaner and faster
+// console.log(Object.keys(Laptop));
+// console.log(Object.values(Laptop));
+
+//cloning an object uspng (spread oo)
+let car1={
+    make: 'toyota',
+    year: 2023
 }
-let person1=person('Peter','Mukanya',3,60,500)
-console.log(person1);
-let person2=person('John','Mukanya',1,20,350)
-console.log(person2);
+// let car2 =car1  //clones everything including reference number
+// console.log("==car1==");
+// console.log(car1);
+// console.log("==car2==");
+// console.log(car2);
+// console.log("Change the value of car2");
+// car2.year=2024
+// console.log(car1,car2);
+// to fix the problem above(cloning reference too) which means when you change anything in car 2 it changes 1 too ---spread operator,object.assign(),JSON.parse()
 
-//constructive function allows us to create an object.Then return this
-//name of fuction must be uppercase
-function Laptop(make,amount){
-    this.make=make
-    this.amount=amount
-    // it does this(JS)==> return this
-}
+// let car2={...car1} //spread operator
+// console.log(car1,car2);
+// console.log("Change the value of car2")
+// car2.year=2024
+// console.log(car1,car2);
 
-let laptop1= new Laptop ('HP',18000)
-let laptop2= new Laptop('Dell',20000)
-console.log(laptop1);
-console.log(laptop2);
-//check for DNA
-console.log(laptop2 instanceof Laptop)
+// //object.assign(datatype,source)
+// let car3= Object.assign({},car1)
+// console.log("===car3===");
+// console.log(car3);
+
+// //JSON.parse its like parseint   converts object to string and creates a new one
+// console.log("JSON.parse");
+// let car4= JSON.parse(JSON.stringify(car1))
+// console.log(car4);
+
+//map modifies array elements(cam)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// String method
+let firstName = 'Joel'
+console.log(firstName.split("").reverse().join(""));
+console.log(firstName.charAt(1));
+console.log(firstName.endsWith("l"));
+console.log(firstName.includes("p"));
+console.log(firstName.lastIndexOf("e"));
+console.log(firstName.replace("l","t"));
+console.log(firstName.repeat(5));
+console.log(firstName.endsWith("n"));
+console.log(firstName.split(''));
+/*
+Exercises on the below methods
+- charAt()
+- endsWith()
+- includes()
+- lastIndexOf()
+- indexOf()
+- replace()
+- repeat()
+- startWith()
+- subString()
+- trim()
+- trimEnd()
+- trimStart()
+- slice()
+- split()
+*/
+
+// let numbers = [2, 4, 11, 1]
+// let a = 3
+// let b = 2
+// console.log(`Highest number: ${Math.max(...numbers)}`);
+// console.log(`Lowest number: ${Math.min(...numbers)}`);
+// // 3^2 = 3 * 3
+// console.log(`pow(x,y): ${Math.pow(a, b)}`);
+// console.dir(Math);
